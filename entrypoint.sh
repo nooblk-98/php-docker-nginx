6779e533f17913da814ede992b747e64f1222fc4
NoobLk <125950957+nooblk-98@users.noreply.github.com>
2025-06-07 22:02:36 +0530

    supervisor update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# Start supervisor services (e.g., nginx, php-fpm)
+# Start supervisor services (ginx, php-fpm)
 exec supervisord -c /etc/supervisord.conf
-
-# # Start PHP-FPM
-# echo "Starting PHP-FPM..."
-# php-fpm -D
-
-# # Start Nginx in foreground
-# echo "Starting Nginx..."
-# exec nginx -g "daemon off;"
